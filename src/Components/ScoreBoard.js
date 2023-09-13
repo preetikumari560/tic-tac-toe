7efaf2732ce04f45d294d15bf01169125464f0c4
preetikumari560 <preetigoswami525@gmail.com>
2023-09-13 19:29:33 +0530

    initial commit

---
+++ src/Components/ScoreBoard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ScoreBoard.css";
+
+const ScoreBoard = ({ xScore, oScore, xPlaying, tie }) => {
+  return (
+    <div className="scoreboard">
+      <span className={`x-score ${xPlaying === true ? "xScoreboard" : ""}`}>
+        X - {xScore}
+      </span>
+     
+      <span className={`o-tie ${tie > 0 ? "oTie" : ""}`}>Tie - {tie}</span>
+
+       <span className={`o-score ${xPlaying === false ? "oScoreboard" : ""}`}>
+        O - {oScore}
+      </span>
+    </div>
+  );
+};
+
+export default ScoreBoard;
